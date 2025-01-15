93f5a196486d744d22ffc288952f30a8ab2ee332
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-15 22:26:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const makeChange = (c) => {
   // your name here
+	s = (c/100)
+	ansobj = {}
+
+	const obj = 
+		   {
+			0:["q" , 0.25],
+			1:["d" , 0.10],
+			2:["n" , 0.05],
+			3:["p" , 0.01],
+		   }
+			
+	l  =	Object.keys(obj).length
+	i =0
+	j = 0
+	while(i<l && j<4 )
+	{
+	    const [curr,x] = obj[i]
+	    ct = 0
+	    
+	    while(x<=s)
+	    {
+	        ct++
+	        s-=x
+	    }
+	    ansobj[curr]  = ct
+	    
+	    i++
+	   
+	}
+	return ansobj
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
